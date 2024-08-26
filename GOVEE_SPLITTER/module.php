9cd05f313fb9e0e8ae150fbd10f0f2b30ac24400
janpeterdietz <jan-peter.dietz@web.de>
2024-08-26 15:21:45 +0200

    Update module.php

--- GOVEE_SPLITTER/module.php
+++ GOVEE_SPLITTER/module.php
@@ -25,12 +25,9 @@ declare(strict_types=1);
 
 			$this->GetConfigurationForParent();
 
-			//print_r(IPS_GetConfiguration(29207));
-
-
 		}
 
-		public function GetConfigurationForParent()
+		public function GetConfigurationForParent() //Set UBD Port
         {
             $settings = [
                 'BindPort'           => 4002,
@@ -44,24 +41,16 @@ declare(strict_types=1);
 
             return json_encode($settings, JSON_UNESCAPED_SLASHES);
 
-			//{"BindIP":"192.168.3.44","BindPort":4003,"EnableBroadcast":false,"EnableReuseAddress":false,"Host":"","Open":true,"Port":0}
 		}
 
-
   
-		
-			
-
-  
-
+	
 		public function ForwardData($JSONString)
 		{
 			$data = json_decode($JSONString);
 			//IPS_LogMessage('Splitter FRWD', utf8_decode($data->Buffer . ' - ' . $data->ClientIP . ' - ' . $data->ClientPort));
 
 			$this->SendDataToParent(json_encode([
-				//'DataID' => '{C8792760-65CF-4C53-B5C7-A30FCC84FEFE}', 
-				//'Type' => 0, 
 		
 				'DataID' => '{8E4D9B23-E0F2-1E05-41D8-C21EA53B8706}', 
 
