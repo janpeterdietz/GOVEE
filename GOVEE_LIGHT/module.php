63a1ea0892f46d7b6f1b0e81f8f62486b1300e72
janpeterdietz <jan-peter.dietz@web.de>
2024-08-26 15:21:52 +0200

    Update module.php

--- GOVEE_LIGHT/module.php
+++ GOVEE_LIGHT/module.php
@@ -86,11 +86,9 @@ declare(strict_types=1);
 			//$data = json_decode($JSONString);
         	
 			$data = json_decode($JSONString);
-			//IPS_LogMessage('Device RECV', utf8_decode($data->Buffer) . ' - ' . $data->ClientIP . ' - ' . $data->ClientPort);
+			IPS_LogMessage('Device RECV', utf8_decode($data->Buffer) . ' - ' . $data->ClientIP . ' - ' . $data->ClientPort);
 			if ($data->ClientIP == $this->ReadPropertyString("IPAddress"))
 			{
-				//$buffer = json_decode($data['Buffer'], true);
-							
 				$buffer = json_decode($data->Buffer, true);
 							
 				$deviceData = $buffer['msg']['data'];
