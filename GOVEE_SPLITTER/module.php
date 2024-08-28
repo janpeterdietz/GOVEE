9d1fb87bd575dd0bedf17d6c2e6e98b5b680cc2c
janpeterdietz <jan-peter.dietz@web.de>
2024-08-28 11:21:30 +0200

    Update module.php

--- GOVEE_SPLITTER/module.php
+++ GOVEE_SPLITTER/module.php
@@ -24,7 +24,8 @@ declare(strict_types=1);
 			parent::ApplyChanges();
 			$this->GetConfigurationForParent();
 
-			echo IPS_GetConfiguration(IPS_GetInstance($this->InstanceID)['ConnectionID']);
+			$config = json_encode( IPS_GetConfiguration(IPS_GetInstance($this->InstanceID)['ConnectionID']), true);
+			echo $config['BindIP']. $config['BindPort']; 
 			//$IPAddress=$this->ReadPropertyString("IPAddress");
 			//$this->SetSummary($IPAddress);
 		}
