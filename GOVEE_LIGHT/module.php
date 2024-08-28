5c7619791d7682532aceaa9c70d2d5104a31bbd4
janpeterdietz <jan-peter.dietz@web.de>
2024-08-28 09:17:31 +0200

    Update module.php

--- GOVEE_LIGHT/module.php
+++ GOVEE_LIGHT/module.php
@@ -63,8 +63,8 @@ declare(strict_types=1);
 			$this->SetSummary($IPAddress);
 
 			//$this->SetReceiveDataFilter($IPAddress);
-			$this->SetReceiveDataFilter('"msg"');
-			
+			$this->SetReceiveDataFilter('"/192/mu"');
+				
 		}
 
 		public function Send()
