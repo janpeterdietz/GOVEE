0e90380239e88f242be7e163f272974f9a2d61ee
janpeterdietz <jan-peter.dietz@web.de>
2024-08-28 08:55:59 +0200

    Update module.php

--- GOVEE_LIGHT/module.php
+++ GOVEE_LIGHT/module.php
@@ -63,7 +63,7 @@ declare(strict_types=1);
 			$this->SetSummary($IPAddress);
 
 			//$this->SetReceiveDataFilter($IPAddress);
-			$this->SetReceiveDataFilter('*"ClientIP":*"');
+			$this->SetReceiveDataFilter('*"ClientIP":*/i"');
 			//
 		}
 
