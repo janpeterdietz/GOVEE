260aa3e727aee148836cf26aac7daecad59a3aca
Jan Peter Dietz <jan.peter.dietz@cariad.technology>
2024-09-05 10:59:13 +0200

    Revert "Update module.php"
    This reverts commit 3fbdae66161c2c16db946e9e1074b94e51525d34.

--- GOVEE Konfigurator/module.php
+++ GOVEE Konfigurator/module.php
@@ -58,37 +58,24 @@ declare(strict_types=1);
 				$count = $count+1;
 			}
 			
-			$no_newdevices = $count-1;
 
-			
-			$count = 0;
 			foreach (IPS_GetInstanceListByModuleID('{E1C6AE31-06E8-74DF-CE5F-6DE9A7AED29D}') as $instanceID)
-			{	
-				IPS_LogMessage('Govee Configurator', $instanceID);
+			{
 				
-				//foreach($availableDevices as  $newdevice)
-		
-				/*{
-					//if ( $availableDevices[$count]['IPAddress'] == IPS_GetProperty($instanceID,'IPAddress') )
+				$count = 0;
+				foreach($availableDevices as  $device)
+				{	
+					if ( $availableDevices[$count]['IPAddress'] == IPS_GetProperty($instanceID,'IPAddress') )
 					{
 						$availableDevices[$count]['instanceID'] = $instanceID;
 						$availableDevices[$count]['deviceactive'] = IPS_GetProperty($instanceID,'Active' );
 						$availableDevices[$count]['timerinterval'] = IPS_GetProperty($instanceID,'Interval' );
 						$availableDevices[$count]['name'] = IPS_GetName($instanceID);	
 					}
-			
-							else
-					{
-						$availableDevices[$count+$no_newdevices-1]['instanceID'] = $instanceID;
-						$availableDevices[$count+$no_newdevices-1]['deviceactive'] = IPS_GetProperty($instanceID,'Active' );
-						$availableDevices[$count+$no_newdevices-1]['timerinterval'] = IPS_GetProperty($instanceID,'Interval' );
-						$availableDevices[$count+$no_newdevices-1]['name'] = IPS_GetName($instanceID);	
-					}
-					
-				}*/
-				$count = $count+1;
+					$count = $count+1;
+				}	
+				
 			}
-
 
 			if (count($availableDevices) == 0)
 			{
@@ -102,15 +89,11 @@ declare(strict_types=1);
 					[
 						'type' => 'Configurator', 
 						'caption'=> 'Govee Konfigurator',
-						'sort' => [
-							'column' => 'IPAddress',
-							'direction' => 'ascending'
-						],
 						'delete' => true,
 						'columns' => [
 								[
 									'name' => 'name',
-									'caption' => 'Name',	
+									'caption' => 'Name',
 									'width' => 'auto'
 								],
 								[
