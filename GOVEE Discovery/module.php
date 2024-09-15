c319cb9de8c19da3c9c76b89ed55725ed81c7534
janpeterdietz <jan-peter.dietz@web.de>
2024-09-15 22:38:26 +0200

    Update module.php

--- GOVEE Discovery/module.php
+++ GOVEE Discovery/module.php
@@ -105,7 +105,6 @@ declare(strict_types=1);
 			
 			foreach($newdevices as $key => $device)
 			{
-				//IPS_LogMessage('Govee Configurator', $key);
 			
 				$availableDevices[$count] = 
 					[
@@ -124,7 +123,7 @@ declare(strict_types=1);
 			}
 			
 			$no_new_devices = $count; 
-
+			/*
 			$count = 0; // 
 			foreach (IPS_GetInstanceListByModuleID('{E1C6AE31-06E8-74DF-CE5F-6DE9A7AED29D}') as $instanceID)
 			{
@@ -160,7 +159,7 @@ declare(strict_types=1);
 					$count = $count+1;
 				}
 			}
-
+			*/
 			if (count($availableDevices) == 0)
 			{
 				$availableDevices[$count]['name'] = 'no devices found';	
